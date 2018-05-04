ffdca03081a15e6a6181d25e8e0b3de8a5620c49
bnovakovic <bojanche@gmail.com>
2018-05-04 21:37:47 +0200

    Initial commit

---
+++ android/src/com/topdowncar/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.topdowncar.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.topdowncar.game.CarGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new CarGame(), config);
+	}
+}
---
+++ desktop/src/com/topdowncar/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.topdowncar.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.topdowncar.game.CarGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CarGame(), config);
+	}
+}
