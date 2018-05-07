ef7e24e4475943313c0c9b61cb81f9c659a7ce25
bnovakovic <bojanche@gmail.com>
2018-05-07 19:44:22 +0200

    Cars are moving and drifting now

--- core/src/com/topdowncar/game/screens/PlayScreen.java
+++ core/src/com/topdowncar/game/screens/PlayScreen.java
@@ -1,10 +1,12 @@
 package com.topdowncar.game.screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
@@ -18,6 +20,21 @@ import static com.topdowncar.game.Constants.PPM;
 
 public class PlayScreen implements Screen {
 
+    private static final int DRIVE_DIRECTION_NONE = 0;
+    private static final int DRIVE_DIRECTION_FORWARD = 1;
+    private static final int DRIVE_DIRECTION_BACKWARD = 2;
+
+    private static final int TURN_DIRECTION_NONE = 0;
+    private static final int TURN_DIRECTION_LEFT = 1;
+    private static final int TURN_DIRECTION_RIGHT = 2;
+
+    private static final float DRIFT = 0.99f;
+    private static final float TURN_SPEED = 2.0f;
+    private static final float DRIVE_SPEED = 120.0f;
+    private static final float MAX_SPEED = 35.0f;
+
+
+
     private final SpriteBatch mBatch;
     private final World mWorld;
     private final Box2DDebugRenderer mB2dr;
@@ -25,6 +42,9 @@ public class PlayScreen implements Screen {
     private final Viewport mViewport;
     private final Body mPlayer;
     private final MapLoader mMapLoader;
+
+    private int mDriveDirection = DRIVE_DIRECTION_NONE;
+    private int mTurnDirection = TURN_DIRECTION_NONE;
 
     public PlayScreen() {
         mBatch = new SpriteBatch();
@@ -35,6 +55,8 @@ public class PlayScreen implements Screen {
         mViewport = new FitViewport(640 / PPM, 480 / PPM, mCamera);
         mMapLoader = new MapLoader(mWorld);
         mPlayer = mMapLoader.getPlayer();
+
+        mPlayer.setLinearDamping(0.5f);
     }
 
     @Override
@@ -46,10 +68,84 @@ public class PlayScreen implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(0,0,0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
+        handleInput();
+        processInput();
         update(delta);
+        handleDrift();
         draw();
 
+    }
+
+    private void handleDrift() {
+        Vector2 forwardSpeed = getForwardVelocity();
+        Vector2 lateralSpeed = getLateralVelocity();
+        mPlayer.setLinearVelocity(forwardSpeed.x + lateralSpeed.x * DRIFT, forwardSpeed.y + lateralSpeed.y * DRIFT);
+    }
+
+    private void processInput() {
+        Vector2 baseVector = new Vector2(0,0);
+
+        if (mTurnDirection == TURN_DIRECTION_RIGHT){
+            mPlayer.setAngularVelocity(-TURN_SPEED);
+        } else if (mTurnDirection == TURN_DIRECTION_LEFT){
+            mPlayer.setAngularVelocity(TURN_SPEED);
+        } else if (mTurnDirection == TURN_DIRECTION_NONE && mPlayer.getAngularVelocity() != 0){
+            mPlayer.setAngularVelocity(0.0f);
+        }
+
+        if (mDriveDirection == DRIVE_DIRECTION_FORWARD){
+            baseVector.set(0, DRIVE_SPEED);
+        } else if (mDriveDirection == DRIVE_DIRECTION_BACKWARD) {
+            baseVector.set(0, -DRIVE_SPEED);
+        }
+
+        if (!baseVector.isZero() && mPlayer.getLinearVelocity().len() < MAX_SPEED){
+            mPlayer.applyForceToCenter(mPlayer.getWorldVector(baseVector), true);
+        }
+    }
+
+    private Vector2 getForwardVelocity(){
+        Vector2 currentNormal = mPlayer.getWorldVector(new Vector2(0, 1));
+        float dotProduct = currentNormal.dot(mPlayer.getLinearVelocity());
+        return multiply(dotProduct, currentNormal);
+    }
+
+    private Vector2 getLateralVelocity(){
+        Vector2 currentNormal = mPlayer.getWorldVector(new Vector2(1, 0));
+        float dotProduct = currentNormal.dot(mPlayer.getLinearVelocity());
+        return multiply(dotProduct, currentNormal);
+    }
+
+    private Vector2 multiply(float a, Vector2 v){
+        return new Vector2(a * v.x, a * v.y);
+    }
+
+    private void handleInput() {
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)){
+            mDriveDirection = DRIVE_DIRECTION_FORWARD;
+        } else if (Gdx.input.isKeyPressed(Input.Keys.DOWN)){
+            mDriveDirection = DRIVE_DIRECTION_BACKWARD;
+        } else {
+            mDriveDirection = DRIVE_DIRECTION_NONE;
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)){
+            mTurnDirection = TURN_DIRECTION_LEFT;
+        } else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
+            mTurnDirection = TURN_DIRECTION_RIGHT;
+        } else {
+            mTurnDirection = TURN_DIRECTION_NONE;
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.ESCAPE)){
+            Gdx.app.exit();
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.Q)){
+            mCamera.zoom -= 0.4f;
+        } else if (Gdx.input.isKeyPressed(Input.Keys.E)){
+            mCamera.zoom += 0.4f;
+        }
     }
 
     private void draw() {
